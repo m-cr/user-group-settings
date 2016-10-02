c9694483d39338cf307d4010047193ab6b75a28f
pspeiser <pat@patspeiser.com>
2016-10-02 10:50:01 -0400

    basic state set up

--- browser/app.js
+++ browser/app.js
@@ -1,1 +1,18 @@
-angular.module('UGS', []).
+angular.module('UGS', ['ui.router']);
+
+angular.module('UGS')
+	.config(function($stateProvider, $urlRouterProvider){
+		$stateProvider
+			.state('home',{
+				url: '/',
+				templateUrl: 'index.html'
+			})
+			.state('users', {
+				url: '/users',
+				templateUrl: '/users/user.html'
+			})
+			.state('groups',{
+				url: '/groups',
+				templateUrl: '/groups/group.html' 
+			});
+	});
