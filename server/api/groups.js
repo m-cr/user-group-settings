16a38c8747f8b083c65ea7284d6a5a3be7ac7b2e
Michael Crowley <miketcrowley@gmail.com>
2016-10-02 10:40:52 -0400

    starting on routes

---
+++ server/api/groups.js
@@ -0,0 +1,3 @@
+var router = require('express').Router();
+var Group = require('../db').models.Group;
+
---
+++ server/api/users.js
@@ -0,0 +1,3 @@
+var router = require('express').Router();
+var User = require('../db').models.User;
+
